b682cbeb25a657df2cadc9560c1be97fc2c93f8b
David Carter <dajocarter@gmail.com>
2018-10-22 16:08:48 -0700

    Transition from css and inline styles to styled-components

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const Container = styled.header`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+
+const Logo = styled.h1`
+  margin: 0;
+
+  a {
+    color: white;
+    text-decoration: none;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Container>
+    <Wrapper>
+      <Logo>
+        <Link to="/">{siteTitle}</Link>
+      </Logo>
+    </Wrapper>
+  </Container>
 )
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,9 +2,21 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import styled, { createGlobalStyle } from 'styled-components'
+import styledNormalize from 'styled-normalize'
 
 import Header from './header'
-import './layout.css'
+
+const GlobalStyles = createGlobalStyle`
+  ${styledNormalize}
+`
+
+const SiteWrapper = styled.main`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0px 1.0875rem 1.45rem;
+  padding-top: 0;
+`
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -28,17 +40,9 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
+        <GlobalStyles />
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+        <SiteWrapper>{children}</SiteWrapper>
       </>
     )}
   />
